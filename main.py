e6f34e10d5dab664e4ddecce547fbcf2a9b45e89
GhosTheKaos3150 <uriel_anderson@hotmail.com>
2022-05-10 11:59:13 -0300

    Ultimas Atualizações

--- main.py
+++ main.py
@@ -1,31 +1,82 @@
 import re
 
+TABELAS = [
+    {
+        "nome": "usuario",
+        "colunas": ["idusuario", "nome", "logradouro", "numero", "bairro", "cep", "uf", "datanascimento"],
+        "relac": []
+    },
+    {
+        "nome": "contas",
+        "colunas": ["idconta", "descricao", "tipoconta_idtipoconta", "usuario_idusuario", "saldoinicial"],
+        "relac": []
+    },
+    {
+        "nome": "tipoconta",
+        "colunas": ["idtipoconta", "descricao"],
+        "relac": []
+    },
+    {
+        "nome": "movimentacao",
+        "colunas": ["idmovimentacao", "datamovimentacao", "descricao", "tipomovimento_idtipomovimento", "categoria_idcategoria", "contas_idconta", "valor"],
+        "relac": []
+    },
+    {
+        "nome": "tipomovimento",
+        "colunas": ["idtipomovimento", "descmovimentacao"],
+        "relac": []
+    },
+    {
+        "nome": "categoria",
+        "colunas": ["idcategoria", "descategoria"],
+        "relac": []
+    }
+]
 
-entrada = "SELECT COLUNA1,COLUNA2 FROM TABELA1".lower()
+
+entrada = "SELECT idCategoria, desCategoria FROM Categoria JOIN movimentacao WHERE numero > 0".lower()
 # print(entrada)
 
+def readCommand(entrada):
+    print("\nCOMANDO: " + entrada)
 
-columns = re.search('select(.*)from', entrada).group(1)
-print("\n COLUNAS IDENTIFICADAS : "+columns)
+    tables = re.search('from (.*)', entrada).group(1).replace(" join", ",")
+    print("\n TABELAS IDENTIFICADAS : "+ str(tables.split(", ")))
 
-if("join" in entrada):
-    tables = re.search('from(.*)inner', entrada).group(1)
-    print("\n TABELAS IDENTIFICADAS : "+tables)
+    columns = re.search('select (.*) from', entrada).group(1)
+    print("\n COLUNAS IDENTIFICADAS : "+str(columns.split(", ")))
+
+    where_cond = re.search('from (.*)', entrada).group(1).replace(" join", ",")
+    print("\n TABELAS IDENTIFICADAS : "+ str(where_cond.split(", ")))
+
+    tabelaExiste = False
+    for tabela in TABELAS:
+        if tabela["nome"].lower() in tables.split(", "):
+            tabelaExiste = True
+            print("tabela identificada\n")
+            for coluna in columns.split(","):
+                if coluna.strip() not in tabela["colunas"]:
+                    print(f"coluna '{coluna.strip()}' NAO ASLKDNSLAJKND em '{tabela['nome']}'")
+                else:
+                    print(f"coluna '{coluna.strip()}' EXISTE em '{tabela['nome']}'")
+    if not tabelaExiste:
+        print("Tabela Não EXISTE")
 
     
-    conditionals = re.search('on(.*)', entrada).group(1)
-    print("\n CONDIÇÕES IDENTIFICADAS : "+conditionals)
+    
+    saida_atual = "π " + columns + "(" + " (" + tables + "))"
+    print("\nSAIDA: " + saida_atual)
+    #conditionals = re.search('where(.*)', entrada).group(1)
+    #print("\n CONDIÇÕES IDENTIFICADAS : "+conditionals)
 
-else:
-    tables = re.search('from(.*)where', entrada).group(1)
-    print("\n TABELAS IDENTIFICADAS : "+tables)
+#saida_atual = "π" + columns + "(σ" + conditionals + " (" + tables1|X| tabela2 + "))"
 
-    conditionals = re.search('where(.*)', entrada).group(1)
-    print("\n CONDIÇÕES IDENTIFICADAS : "+conditionals)
-
-saida_atual = "π" + columns + "(σ" + conditionals + " (" + tables + "))"
 saida_ideal = "π COLUNA1, COLUNA2(σ SEXO=M(TABELA1))".lower()
-print("\n VERSAO ALGEBRICA: "+ saida_atual)
+
+
+readCommand(entrada)
+
+
 # projecao π -> SELECT
 # selecao σ -> WHERE
 # ^ v -> AND OR
